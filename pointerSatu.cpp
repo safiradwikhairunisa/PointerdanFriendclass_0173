a65fab997d0b6cc6380f96f4bf50547e4ed9db3c
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-05-05 15:27:43 +0700

    menambah intejer

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -7,4 +7,11 @@ class Mahasiswa{
     void showNim(){
         cout << "No induk = " << nim << endl;
     }
+};
+
+int nim(){
+
+    mahasiswa mhs; //objek mhs
+    mhs.nim = 5;
+    mhs.showNim(); //member akses 
 }
